2f68aeee541121426e6e1380495c40a102c7b1a3
kt <98679116+Kartikey-Tiwari@users.noreply.github.com>
2022-11-03 22:34:01 +0530

    Feature: Calculator works

--- script.js
+++ script.js
@@ -32,36 +32,36 @@ function operate(a, operator, b=0){
 }
 
 let result = 0;
+let curNumber = 0;
+let operator = '';
 let wasLastOperator = false;
 let isFloat = false;
-let wasLastNumFloat = false;
+let display = document.querySelector('#display');
+let resultExpr = document.querySelector('#result-expr')
 
 const buttons = document.querySelectorAll('button');
 buttons.forEach(button => {
 	button.addEventListener('click', (event) => {
 		if (button.id==='all-clear'){
-			display.textContent = ''
+			display.textContent = '0'
+      operator = '';
 			result = 0;
+      resultExpr.textContent = '';
+      isFloat = false;
 		}
 		else if (button.id==='clear'){
-			if (display.textContent.length === 1){
-				display.textContent = '0';
-			}
-			else {
-				if (/^[+\-\%\÷x]$/.test(display.textContent[display.textContent.length-1])){
-					wasLastOperator = false;
-          isFloat = wasLastNumFloat;
-          wasLastNumFloat = false;
-				}
-        else if (display.textContent[display.textContent.length-1] === '.') {
+      if (display.textContent){
+        if (display.textContent[display.textContent.length-1] === '.') {
           isFloat = false;
         }
 				display.textContent = display.textContent.slice(0, display.textContent.length-1);
-			}
+      }
 		}
     else if (button.id === '.'){
       if (!isFloat){
         isFloat = true;
+        if (!display.textContent)
+          display.textContent += '0';
         display.textContent += '.';
       }
     }
@@ -76,17 +76,46 @@ buttons.forEach(button => {
 			}
 			else{
 				if (button.classList.contains('operator-btn')){
-					if (wasLastOperator){
-						display.textContent = display.textContent.slice(0, display.textContent.length-1) + button.id;
+          if (operator === ""){
+            operator = button.id;
+            resultExpr.textContent = display.textContent + button.id; 
+            result = +display.textContent;
+            display.textContent = "";
+          }
+          else if (wasLastOperator && operator !== '%'){
+            operator = button.id;
+						resultExpr.textContent = resultExpr.textContent.slice(0, resultExpr.textContent.length-1) + button.id;
 					}
-					else{
-						display.textContent += button.id;
-            wasLastNumFloat = isFloat;
-					}
+          else{
+            curNumber = +display.textContent;
+            result = operate(result, operator, curNumber);
+            operator = button.id;
+            resultExpr.textContent = result + button.id;
+            display.textContent = "";
+          }
 					wasLastOperator = true;
           isFloat = false;
 				}
+        else if (button.id === '='){
+          if (resultExpr.textContent !== ''){
+            if (operator){
+              curNumber = +display.textContent;
+              if (!curNumber && operator !== '%')
+                return;
+              result = operate(result, operator, curNumber);
+              if (operator === '%')
+                curNumber = '';
+              resultExpr.textContent += curNumber + '=';
+              display.textContent = result;
+            }
+          }
+          operator = '';
+        }
 				else{
+          if (resultExpr.textContent[resultExpr.textContent.length-1] === '=')
+            resultExpr.textContent = '';
+          if (operator === '%')
+            return;
 					wasLastOperator = false;
 					display.textContent += button.id;
 				}
