e9fb8100617162da0d81f9fdb6f2f86f7769a50f
kt <98679116+Kartikey-Tiwari@users.noreply.github.com>
2022-11-06 14:06:20 +0530

    Add keyboard support and refactoring

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+let result = 0;
+let curNumber = 0;
+let operator = "";
+let wasLastOperator = false;
+let isFloat = false;
+let display = document.querySelector("#display");
+let resultExpr = document.querySelector("#result-expr");
+let allClearArr = {
+  Shift: false,
+  Backspace: false,
+};
+
 function add(a, b) {
   return a + b;
 }
@@ -40,121 +52,175 @@ function roundResult() {
   }
   if (lenResult > 14) {
     result = result.toPrecision(14);
+    if (result % 1 === 0) {
+      result = parseFloat(result);
+    }
   }
 }
 
-let result = 0;
-let curNumber = 0;
-let operator = "";
-let wasLastOperator = false;
-let isFloat = false;
-let display = document.querySelector("#display");
-let resultExpr = document.querySelector("#result-expr");
+function handleDigitInput(digit) {
+  if (display.textContent === "Infinity") return;
+  if (resultExpr.textContent[resultExpr.textContent.length - 1] === "=")
+    resultExpr.textContent = "";
+  if (operator === "%") return;
+  wasLastOperator = false;
+  if (display.textContent === "" && digit === "00") {
+    display.textContent = "0";
+  } else if (display.textContent === "0") {
+    if (digit === "00") return;
+    else display.textContent = digit;
+  } else {
+    if (
+      display.textContent.length < 14 ||
+      (display.textContent[0] === "-" && display.textContent.length < 15)
+    )
+      display.textContent += digit;
+  }
+}
+
+function calculateResult() {
+  curNumber = +display.textContent;
+  result = operate(result, operator, curNumber);
+  roundResult();
+}
+
+function clearDisplay() {
+  if (display.textContent) {
+    if (display.textContent === "Infinity") {
+      display.textContent = "";
+    }
+    if (display.textContent[display.textContent.length - 1] === ".") {
+      isFloat = false;
+    }
+    if (!operator) {
+      resultExpr.textContent = "";
+    }
+    display.textContent = display.textContent.slice(
+      0,
+      display.textContent.length - 1
+    );
+    if (!display.textContent) {
+      if (
+        resultExpr.textContent &&
+        /[+-\÷x]/.test(
+          resultExpr.textContent[resultExpr.textContent.length - 1]
+        )
+      ) {
+        wasLastOperator = true;
+      }
+    }
+  }
+}
+
+function handleOperatorInput(op) {
+  if (display.textContent === "" && resultExpr.textContent === "") {
+    if (op === "-") {
+      display.textContent = op;
+    }
+    return;
+  } else if (operator === "") {
+    operator = op;
+    resultExpr.textContent = display.textContent + op;
+    result = +display.textContent;
+    display.textContent = "";
+  } else if (wasLastOperator && operator !== "%") {
+    if ((operator === "x" || operator === "÷") && op === "-") {
+      display.textContent = op;
+    } else {
+      operator = op;
+      resultExpr.textContent =
+        resultExpr.textContent.slice(0, resultExpr.textContent.length - 1) + op;
+    }
+  } else {
+    calculateResult();
+    operator = op;
+    resultExpr.textContent = result + op;
+    result = +result;
+    display.textContent = "";
+  }
+  wasLastOperator = true;
+  isFloat = false;
+}
+
+function handleEqualsInput() {
+  if (resultExpr.textContent !== "") {
+    if (operator) {
+      if (display.textContent === "" && operator !== "%") return;
+      calculateResult();
+      resultExpr.textContent += curNumber + "=";
+      display.textContent = result;
+    }
+  }
+  operator = "";
+}
+
+function handleDotInput() {
+  if (!isFloat) {
+    isFloat = true;
+    if (!display.textContent) display.textContent += "0";
+    display.textContent += ".";
+    if (operator === "") resultExpr.textContent = "";
+  }
+}
+
+function allClear() {
+  display.textContent = "";
+  operator = "";
+  result = 0;
+  resultExpr.textContent = "";
+  isFloat = false;
+}
 
 const buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
   button.addEventListener("click", (event) => {
     if (button.id === "all-clear") {
-      display.textContent = "";
-      operator = "";
-      result = 0;
-      resultExpr.textContent = "";
-      isFloat = false;
+      allClear();
     } else if (button.id === "clear") {
-      if (display.textContent) {
-        if (display.textContent === "Infinity") {
-          display.textContent = "";
-        }
-        if (display.textContent[display.textContent.length - 1] === ".") {
-          isFloat = false;
-        }
-        if (!operator) {
-          resultExpr.textContent = "";
-        }
-        display.textContent = display.textContent.slice(
-          0,
-          display.textContent.length - 1
-        );
-        if (!display.textContent) {
-          if (
-            resultExpr.textContent &&
-            /[+-\÷x]/.test(
-              resultExpr.textContent[resultExpr.textContent.length - 1]
-            )
-          ) {
-            wasLastOperator = true;
-          }
-        }
-      }
+      clearDisplay();
     } else if (button.id === ".") {
-      if (!isFloat) {
-        isFloat = true;
-        if (!display.textContent) display.textContent += "0";
-        display.textContent += ".";
-        if (operator === "") resultExpr.textContent = "";
-      }
+      handleDotInput();
     } else if (button.classList.contains("operator-btn")) {
-      if (display.textContent === "" && resultExpr.textContent === "") {
-        if (button.id === "-") {
-          display.textContent = button.id;
-        }
-        return;
-      } else if (operator === "") {
-        operator = button.id;
-        resultExpr.textContent = display.textContent + button.id;
-        result = +display.textContent;
-        display.textContent = "";
-      } else if (wasLastOperator && operator !== "%") {
-        if ((operator === "x" || operator === "÷") && button.id === "-") {
-          display.textContent = button.id;
-        } else {
-          operator = button.id;
-          resultExpr.textContent =
-            resultExpr.textContent.slice(0, resultExpr.textContent.length - 1) +
-            button.id;
-        }
-      } else {
-        curNumber = +display.textContent;
-        result = operate(result, operator, curNumber);
-        roundResult();
-        operator = button.id;
-        resultExpr.textContent = result + button.id;
-        result = +result;
-        display.textContent = "";
-      }
-      wasLastOperator = true;
-      isFloat = false;
+      handleOperatorInput(button.id);
     } else if (button.id === "=") {
-      if (resultExpr.textContent !== "") {
-        if (operator) {
-          curNumber = +display.textContent;
-          if (display.textContent === "" && operator !== "%") return;
-          result = operate(result, operator, curNumber);
-          roundResult();
-          if (operator === "%") curNumber = "";
-          resultExpr.textContent += curNumber + "=";
-          display.textContent = result;
-        }
-      }
-      operator = "";
+      handleEqualsInput();
     } else {
-      if (display.textContent === "Infinity") return;
-      if (resultExpr.textContent[resultExpr.textContent.length - 1] === "=")
-        resultExpr.textContent = "";
-      if (operator === "%") return;
-      wasLastOperator = false;
-      if (display.textContent === "" && button.id === "00") {
-        display.textContent = "0";
-      } else if (display.textContent === "0") {
-        if (button.id === "00") return;
-        else display.textContent = button.id;
-      } else {
-        if (
-          display.textContent.length < 14 ||
-          (display.textContent[0] === "-" && display.textContent.length < 15)
-        )
-          display.textContent += button.id;
-      }
+      handleDigitInput(button.id);
     }
   });
+});
+
+window.addEventListener("keydown", (event) => {
+  const key = event.key;
+
+  if (key == +key) {
+    handleDigitInput(key);
+  } else if (key === "Backspace") {
+    clearDisplay();
+  } else if (key === ".") {
+    handleDotInput();
+  } else if (/[+-\/\*\%]/.test(key)) {
+    if (key === "/") {
+      handleOperatorInput("÷");
+      event.preventDefault();
+    } else if (key === "*") handleOperatorInput("x");
+    else handleOperatorInput(key);
+  } else if (key === "Enter" || key === "=") {
+    event.preventDefault();
+    handleEqualsInput();
+  }
+  if (key === "Shift" || key === "Backspace") {
+    allClearArr[key] = true;
+  }
+  if (allClearArr["Shift"] && allClearArr["Backspace"]) {
+    allClear();
+  }
+});
+
+window.addEventListener("keyup", (event) => {
+  const key = event.key;
+
+  if (key === "Shift" || key === "Backspace") {
+    allClearArr[key] = false;
+  }
 });
